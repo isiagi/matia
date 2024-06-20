d1013750de3d78f4c8f779a3d0be844e562c5344
isiagi <isiagigeofrey0@gmail.com>
2024-06-20 12:10:34 +0300

    styling

--- app/components/headerBanner/HeaderBanner.js
+++ app/components/headerBanner/HeaderBanner.js
@@ -5,7 +5,7 @@ function HeaderBanner({ title }) {
   return (
     <div
       className="min-h-[40vh] bg-[url('/assets/images/matia.jpg')] bg-center bg-cover bg-no-repeat bg-black bg-blend-overlay bg-opacity-[0.3]
-  flex items-center"
+  flex items-center bg-fixed"
     >
       <div className="text-white mx-5 leading-loose">
         <h2 className="text-3xl font-medium">{title}</h2>
