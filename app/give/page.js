14198ea4be60782351c7537bbafdf31d24991850
isiagi <isiagigeofrey0@gmail.com>
2024-06-20 11:47:55 +0300

    refactor: Add flutterwave payment

--- app/give/page.js
+++ app/give/page.js
@@ -1,15 +1,42 @@
+"use client";
+
 import React from "react";
 import HeaderBanner from "../components/headerBanner/HeaderBanner";
 import Image from "next/image";
+import { closePaymentModal, useFlutterwave } from "flutterwave-react-v3";
 
-function page() {
+function Page() {
+  const config = {
+    public_key: process.env.NEXT_PUBLIC_WAVE_KEY,
+    tx_ref: Date.now(),
+    amount: 100,
+    currency: "UGX",
+    payment_options: "card,mobilemoney,ussd",
+    customer: {
+      email: "user@gmail.com",
+      phone_number: "070********",
+      name: "john doe",
+    },
+    customizations: {
+      title: "ST MATIA MULUMBA PARISH",
+      description: "Church Donations",
+      logo: "https://st2.depositphotos.com/4403291/7418/v/450/depositphotos_74189661-stock-illustration-online-shop-log.jpg",
+    },
+  };
+
+  const handleFlutterPayment = useFlutterwave(config);
+
   return (
     <div>
       <HeaderBanner title={"Give"} />
       <div className="py-14 mx-5">
         <div className="grid grid-cols-fluid gap-5">
-          {[1, 2, 3].map((_, i) => (
-            <div key={i}>
+          {[
+            { id: 1, buttonName: "Donate" },
+            { id: 2, buttonName: "Give" },
+            { id: 2, buttonName: "Offer" },
+          ].map(({ id, buttonName }) => (
+            <div key={id}>
               <div>
                 <Image
                   src="/assets/images/matia.jpg"
@@ -34,9 +61,21 @@ function page() {
                 </p>
               </div>
               <div>
-                <button className="bg-blue-500 w-full p-2 text-white rounded-lg hover:bg-blue-400">
-                  Give
+                <button
+                  onClick={() => {
+                    handleFlutterPayment({
+                      callback: (response) => {
+                        console.log(response);
+                        closePaymentModal(); // this will close the modal programmatically
+                      },
+                      onClose: () => {},
+                    });
+                  }}
+                  className="bg-blue-500 w-full p-2 text-white rounded-lg hover:bg-blue-400"
+                >
+                  {buttonName}
                 </button>
+                {/* <FlutterWaveButton {...getFwConfig(id, buttonName)} /> */}
               </div>
             </div>
           ))}
@@ -46,4 +85,4 @@ function page() {
   );
 }
 
-export default page;
+export default Page;
